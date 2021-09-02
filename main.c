841d1944c4e8c6747d1bb27e422ef28e231151d0
isaaclim <isaaclimjj@yahoo.com>
2021-09-02 13:48:44 +0800

    added ft_time

--- main.c
+++ main.c
@@ -2,14 +2,39 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <sys/time.h>
+
+int	ft_time(void)
+{
+	struct timeval time;
+
+	gettimeofday(&time, NULL);
+	long millisecond = time.tv_sec % 1000 * 1000 + time.tv_usec / 1000;
+	return ((int)millisecond);
+}
+
+pthread_mutex_t mutex;
+
+void	*threadFuncRet()
+{
+	int *result = malloc(sizeof(int));
+
+	*result = 10;
+	printf("Return add: %p\n", result);
+	return ((void *)result);
+}
 
 int primes[10] = {2,3,5,7,11,13,17,19,23,29};
 
 void	*myThreadFunc(void *arg)
 {
+	int timestamp;
 	int *i = (int *)arg;
-	printf("First line: %d\n", primes[*i]);
+	pthread_mutex_lock(&mutex);
+	timestamp = ft_time();
+	printf("First line: %d Timestamp: %i\n", primes[*i], timestamp);
 	free(arg);
+	pthread_mutex_unlock(&mutex);
 	sleep(1);
 	printf("Second line\n");
 }
@@ -20,8 +45,9 @@ int	main(void)
 	int i;
 
 	printf("Before thread\n");
+	pthread_mutex_init(&mutex, NULL);
 	i = 0;
-	while (i < 10)
+	while (i < 2)
 	{
 		int *a = malloc(sizeof(int));
 		*a = i;
@@ -30,11 +56,20 @@ int	main(void)
 		i++;
 	}
 	i = 0;
-	while (i < 10)
+	while (i < 2)
 	{
 		if (pthread_join(th[i], NULL) != 0)
 			return (1);
 		i++;
 	}
 	printf("After thread\n");
+	pthread_t ret;
+	int *ret_val;
+	if (pthread_create(&ret, NULL, threadFuncRet, NULL) != 0)
+		return (1);
+	if (pthread_join(ret, (void **)&ret_val) != 0)
+		return (1);
+	printf("Return add (main): %p\n", ret_val);
+	free(ret_val);
+	pthread_mutex_destroy(&mutex);
 }

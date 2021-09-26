6d2ec02cb7a116218d6d31223b574a0a540da839
isaaclim <isaaclimjj@yahoo.com>
2021-09-27 01:06:46 +0800

    created general process

--- philo_bonus/main.c
+++ philo_bonus/main.c
@@ -1,33 +1,53 @@
 #include "philo_bonus.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <semaphore.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void	ft_philo_process(sem_t *forks, int id)
+{
+	sem_wait(forks);
+	printf("Help from %d\n", id);
+	sleep(3);
+	sem_post(forks);
+}
 
 int	main()
 {
-	// int id_1;
-	// int id_2;
+	int id_1;
+	int id_2;
 
-	// id_1 = fork();
-	// id_2 = fork();
-	// printf("Hellp from %d-%d\n", id_1, id_2);
 
-	// if (id_1 != 0)
-	// {
-	// 	if (id_2 != 0)
-	// 		printf("Hello from %d-%d\n", id_1, id_2);
-	// 	else
-	// 		printf("Hellp from %d-%d\n", id_1, id_1);
-	// }
-	// else
-	// {
-	// 	if (id_2 != 0)
-	// 		printf("Hellp from %d-%d\n", id_1, id_2);
-	// 	else
-	// 		printf("Hellp from %d-%d\n", id_1, id_1);
-	// }
+	sem_unlink("spoon");
+	sem_t *forks;
+	// char forks_name[5];
 
-	char str[10];
-	char *test;
+	// ft_strncpy(forks_name, "spoon", 5);
+	forks = sem_open("spoon", O_CREAT, 0660, 1);
+	if (forks == SEM_FAILED)
+	{
+		printf("Sem_open failed\n");
+		exit (0);
+	}
+	
+	id_1 = fork();
+	sem_unlink("spoon");
 
-	test = ft_strncpy(str, "Hello", 10);
-	printf("%s\n", str);
-	printf("%s\n", test);
+	if (id_1 == 0)
+	{
+		ft_philo_process(forks, id_1);
+		return (0);
+	}
+	id_2 = fork();
+	if (id_2 == 0)
+	{
+		ft_philo_process(forks, id_2);
+		return (0);
+	}
+
+	waitpid(-1, NULL, 0);
+	sem_close(forks);
 }

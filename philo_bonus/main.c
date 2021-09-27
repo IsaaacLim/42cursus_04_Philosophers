3ce5aeb28aee80622796bccfc40052e40c6adf59
isaaclim <isaaclimjj@yahoo.com>
2021-09-27 19:00:57 +0800

    replaced fixed processes with variable philos

--- philo_bonus/main.c
+++ philo_bonus/main.c
@@ -1,57 +1,49 @@
 #include "philo_bonus.h"
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <semaphore.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+
 
 void	ft_philo_process(sem_t *forks, int id)
 {
 	sem_wait(forks);
+	sem_wait(forks);
 	printf("Help from %d\n", id);
 	sleep(1);
+	sem_post(forks);
 	sem_post(forks);
 }
 
 int	main()
 {
-	int id_1;
-	int id_2;
+	t_philos	*philo;
+	sem_t 		*forks_pointer;
+	int			i;
+	char		forks_name[15];
 
-
-	sem_unlink("spoon");
-	sem_t *forks;
-	// char forks_name[5];
-
-	// ft_strncpy(forks_name, "spoon", 5);
-	forks = sem_open("spoon", O_CREAT, 0660, 1);
-	if (forks == SEM_FAILED)
+	ft_strncpy(forks_name, "fork_semaphore", 15);
+	sem_unlink(forks_name);
+	forks_pointer = sem_open(forks_name, O_CREAT, 0660, 3);
+	if (forks_pointer == SEM_FAILED)
 	{
 		printf("Sem_open failed\n");
 		exit (0);
 	}
+
+	philo = malloc(sizeof(t_philos) * 4);
+	i = -1;
+	while (++i < 4)
+	{
+		philo[i].x = fork();
+		if (philo[i].x == 0)
+		{
+			ft_philo_process(forks_pointer, philo[i].x);
+			return (0);
+		}
+	}
+	i = -1;
+	while (++i < 4)
+	{
+		waitpid(philo[i].x, NULL, 0);
+	}
 	
-	id_1 = fork();
-	sem_unlink("spoon");
-
-	if (id_1 == 0)
-	{
-		ft_philo_process(forks, id_1);
-		return (0);
-	}
-	id_2 = fork();
-	if (id_2 == 0)
-	{
-		ft_philo_process(forks, id_2);
-		return (0);
-	}
-
-	waitpid(-1, NULL, 0);
-	sem_close(forks);
-
-	printf("%d\n", ft_atoi("  -608"));
-	ft_sleep(200);
-	ft_print(3, "Hello", CYAN);
+	sem_close(forks_pointer);
+	free(philo);
 }
--- philo_bonus/philo_bonus.h
+++ philo_bonus/philo_bonus.h
@@ -3,7 +3,13 @@
 
 # include <stdio.h>
 # include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <semaphore.h>
 # include <sys/time.h>
+# include <sys/stat.h>
+# include <sys/wait.h>
+# include <sys/types.h>
 
 # define RED "\033[0;31m"
 # define GREEN "\033[0;32m"
@@ -12,6 +18,13 @@
 # define PURPLE "\033[0;35m"
 # define CYAN "\033[0;36m"
 # define RESET "\033[0;0m"
+
+typedef struct s_philos {
+	int		x;
+	int		t_last_meal;
+	int		n_eaten;
+	char	color[10];
+}	t_philos;
 
 int		ft_atoi(const char *str);
 int		ft_time(void);

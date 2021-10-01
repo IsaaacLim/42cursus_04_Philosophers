eedaacf437f92b64bd8fed2c0ba5e8b4a0b49793
isaaclim <isaaclimjj@yahoo.com>
2021-10-01 17:52:21 +0800

    cleaned utils.c and added comments

--- philo_bonus/utils.c
+++ philo_bonus/utils.c
@@ -1,24 +1,9 @@
 #include "philo_bonus.h"
 
-char *ft_strncpy(char *dest, char *src, unsigned int n)
-{
-	unsigned int i;
-
-	i = 0;
-	while (src[i] != '\0' && i < n)
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	while (i < n)
-	{
-		dest[i] = '\0';
-		i++;
-	}
-	return (dest);
-}
-
-int ft_atoi(const char *str)
+/*
+** Used to store arguments in ft_init_arg
+*/
+int	ft_atoi(const char *str)
 {
 	int		i;
 	int		flag;
@@ -40,6 +25,31 @@ int ft_atoi(const char *str)
 	return (tot * (long)flag);
 }
 
+/*
+** Used to create fork_name (semaphore) in main()
+*/
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
+
+/*
+** Provides time format of: sssmmm
+**	s: seconds , m: milliseconds
+*/
 int	ft_time(void)
 {
 	struct timeval	time;
@@ -50,6 +60,9 @@ int	ft_time(void)
 	return ((int)milliseconds);
 }
 
+/*
+** Allows function to sleep for no longer than the required duration
+*/
 void	ft_sleep(int duration)
 {
 	int	time;
@@ -59,6 +72,9 @@ void	ft_sleep(int duration)
 		usleep(500);
 }
 
+/*
+** Philosopher's status stdout format
+*/
 void	ft_print(t_philos philo, char *str)
 {
 	printf("%06d %s%i %s%s\n", ft_time(), philo.color, philo.x, str, RESET);
